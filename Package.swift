bb450eb3c9f97e0dfa6d1106205bafef2e770f89
Wei Zhang <veeamd@gmail.com>
2020-11-05 11:00:33 -0500

    add some testability and profiling related build settings

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ReactiveObjC",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "ReactiveObjC", targets: ["ReactiveObjC-iOS"])
     ],
